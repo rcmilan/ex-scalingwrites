255559ae054b36b3b0f6818a610119d949df35e4
rcmilan <13418519+rcmilan@users.noreply.github.com>
2025-12-28 18:35:25 -0300

    cleanup

--- ScalingWrites.Core/Controllers/PublicationsController.cs
+++ ScalingWrites.Core/Controllers/PublicationsController.cs
@@ -5,84 +5,83 @@ using ScalingWrites.Core.IO;
 using ScalingWrites.Core.Models;
 using ScalingWrites.Core.Data.Configurations;
 
-namespace ScalingWrites.Core.Controllers
+namespace ScalingWrites.Core.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PublicationsController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class PublicationsController(IReadOnlyList<ShardDescriptor> shards) : ControllerBase
+    [HttpPost]
+    public async Task<ActionResult<PostPublicationOutput>> Post([FromServices] IShardedDbContextFactory contextFactory, [FromBody] PostPublicationInput input)
     {
-        [HttpPost]
-        public async Task<ActionResult<PostPublicationOutput>> Post([FromServices] IShardedDbContextFactory contextFactory, [FromBody] PostPublicationInput input)
+        var publication = new Publication
         {
-            var publication = new Publication
+            Title = input.Title,
+            CreatedAt = DateTime.UtcNow
+        };
+
+        if (input.UserId.HasValue)
+        {
+            await using var db = contextFactory.CreateDbContext(input.UserId.Value);
+            await using var tx = await db.Database.BeginTransactionAsync();
+
+            try
             {
-                Title = input.Title,
-                CreatedAt = DateTime.UtcNow
-            };
+                var user = await db.Users.FindAsync(input.UserId.Value);
+                if (user == null)
+                    return NotFound("User not found");
 
-            if (input.UserId.HasValue)
-            {
-                await using var db = contextFactory.CreateDbContext(input.UserId.Value);
-                await using var tx = await db.Database.BeginTransactionAsync();
+                user.Publications.Add(publication);
+                await db.SaveChangesAsync();
+                await tx.CommitAsync();
 
-                try
-                {
-                    var user = await db.Users.FindAsync(input.UserId.Value);
-                    if (user == null)
-                        return NotFound("User not found");
-
-                    user.Publications.Add(publication);
-                    await db.SaveChangesAsync();
-                    await tx.CommitAsync();
-
-                    return Ok(new PostPublicationOutput(publication.Id));
-                }
-                catch
-                {
-                    await tx.RollbackAsync();
-                    throw;
-                }
+                return Ok(new PostPublicationOutput(publication.Id));
             }
-            else
+            catch
             {
-                await using var db = contextFactory.CreateDbContext(publication.Id);
-                await using var tx = await db.Database.BeginTransactionAsync();
-
-                try
-                {
-                    db.Publications.Add(publication);
-                    await db.SaveChangesAsync();
-                    await tx.CommitAsync();
-
-                    return Ok(new PostPublicationOutput(publication.Id));
-                }
-                catch
-                {
-                    await tx.RollbackAsync();
-                    throw;
-                }
+                await tx.RollbackAsync();
+                throw;
             }
         }
-
-        [HttpGet("{id:guid}")]
-        public async Task<ActionResult<GetPublicationOutput>> Get(
-            [FromServices] IReadOnlyList<ShardDescriptor> shards,
-            [FromServices] IShardedDbContextFactory contextFactory,
-            [FromRoute] Guid id)
+        else
         {
-            foreach (var shard in shards)
+            await using var db = contextFactory.CreateDbContext(publication.Id);
+            await using var tx = await db.Database.BeginTransactionAsync();
+
+            try
             {
-                await using var db = contextFactory.GetShardedDbContext(shard);
+                db.Publications.Add(publication);
+                await db.SaveChangesAsync();
+                await tx.CommitAsync();
 
-                var publication = await db.Publications
-                    .AsNoTracking()
-                    .FirstOrDefaultAsync(p => p.Id == id);
-
-                if (publication != null)
-                    return Ok(new GetPublicationOutput(publication.Id, publication.Title, publication.CreatedAt));
+                return Ok(new PostPublicationOutput(publication.Id));
             }
-
-            return NotFound();
+            catch
+            {
+                await tx.RollbackAsync();
+                throw;
+            }
         }
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<GetPublicationOutput>> Get(
+        [FromServices] IReadOnlyList<ShardDescriptor> shards,
+        [FromServices] IShardedDbContextFactory contextFactory,
+        [FromRoute] Guid id)
+    {
+        foreach (var shard in shards)
+        {
+            await using var db = contextFactory.GetShardedDbContext(shard);
+
+            var publication = await db.Publications
+                .AsNoTracking()
+                .FirstOrDefaultAsync(p => p.Id == id);
+
+            if (publication != null)
+                return Ok(new GetPublicationOutput(publication.Id, publication.Title, publication.CreatedAt));
+        }
+
+        return NotFound();
     }
 }
